8c3efab87a8abd19b448611e59ca91d4abce6c23
Connor Mendenhall <cmendenhall@8thlight.com>
2021-07-02 14:18:34 -0400

    Frontend app (#14)
    * Add create-react-app for frontend
    
    * Start mocking up UI elements
    
    * Mock up form components

---
+++ packages/app/src/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import "@fontsource/pacifico";
+import "@fontsource/damion";
+import "@fontsource/contrail-one";
+import "@fontsource/faster-one";
+import "@fontsource/unifrakturmaguntia";
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
