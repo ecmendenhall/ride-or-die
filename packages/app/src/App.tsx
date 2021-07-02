3f10c4ab51b874b4e094591b6c7e98fc88f1a625
Connor Mendenhall <cmendenhall@8thlight.com>
2021-07-02 12:15:29 -0400

    Mock up form components

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -9,23 +9,35 @@ function App() {
                     <h1 className="font-script text-7xl text-center text-pink-600">🚴‍♀️ Ride or Die 💀</h1>
                 </div>
                 <div className="col-start-2 col-span-4 row-span-3 bg-yellow-50 border border-solid border-gray-300 rounded-lg shadow p-6">
-                    <div className="grid grid-cols-3 gap-4">
+                    <div className="grid grid-cols-3 gap-6">
                         <div>
                             <button className="bg-pink-600 hover:bg-pink-700 rounded-lg shadow py-1 px-2 text-yellow-50">Link Strava</button>
                         </div>
-                        <div className="w-64 grid grid-cols-1 gap-4">
-                            <label className="block">
-                            <span>Distance</span>
-                            <input type="number" className="mt-1 block w-full rounded-md border-gray-300"></input>
-                            </label>
-                            <label className="block">
-                            <span>Deadline</span>
-                            <input type="date" className="mt-1 block w-full rounded-md border-gray-300"></input>
-                            </label>
-                            <label className="block">
-                            <span>Stake</span>
-                            <input type="number" className="mt-1 block w-full rounded-md border-gray-300"></input>
-                            </label>
+                        <div className="max-w-4xl grid grid-cols-1 gap-2">
+                            <div>
+                                <label className="text-pink-600 font-semibold">Distance</label>
+                                <div className="relative flex w-full flex-wrap items-stretch mb-3">
+                                    <input type="text" placeholder="100" className="px-3 py-3 placeholder-black relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full pr-10"/>
+                                    <span className="z-10 h-full leading-snug font-normal absolute text-center absolute bg-transparent rounded text-sm items-center justify-center w-8 right-0 pr-3 py-3">
+                                        km
+                                    </span>
+                                </div>
+                            </div>
+                            <div>
+                                <label className="text-pink-600 font-semibold">Stake</label>
+                                <div className="relative flex w-full flex-wrap items-stretch mb-3">
+                                    <input type="text" placeholder="500" className="px-3 py-3 placeholder-black relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full pr-10"/>
+                                    <span className="z-10 h-full leading-snug font-normal absolute text-center absolute bg-transparent rounded text-sm items-center justify-center w-8 right-0 pr-3 py-3">
+                                        DAI
+                                    </span>
+                                </div>
+                            </div>
+                            <div>
+                                <label className="text-pink-600 font-semibold">Deadline</label>
+                                <div className="relative flex w-full flex-wrap items-stretch mb-3">
+                                    <input type="date" placeholder="mm/dd/yyy" className="px-3 py-3 placeholder-black relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"/>
+                                </div>
+                            </div>
                             <button className="bg-pink-600 hover:bg-pink-700 rounded-lg shadow py-1 px-2 text-yellow-50">Create Goal</button>
                         </div>
                         <div>
--- packages/app/tailwind.config.js
+++ packages/app/tailwind.config.js
@@ -15,6 +15,5 @@ module.exports = {
     extend: {},
   },
   plugins: [
-    require('@tailwindcss/forms'),
   ],
 }
