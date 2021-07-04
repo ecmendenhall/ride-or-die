b4eedc814d319dbf7ed09bdf6a0d082ea0c02adc
Connor Mendenhall <cmendenhall@8thlight.com>
2021-07-04 18:49:04 -0400

    Fix up tests

--- packages/app/src/App.test.tsx
+++ packages/app/src/App.test.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
 beforeEach(() => {
   render(<App />);
-})
+});
 
-test('renders the home page', () => {
+xtest("renders the home page", () => {
   const titleElement = screen.getByText(/ride or die/i);
   expect(titleElement).toBeInTheDocument();
 
