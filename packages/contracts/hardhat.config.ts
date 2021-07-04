ee5059caf2b0964866512c71e2ae0e9471813d05
Connor Mendenhall <cmendenhall@8thlight.com>
2021-07-04 18:38:23 -0400

    Empty defaults for env vars

--- packages/contracts/hardhat.config.ts
+++ packages/contracts/hardhat.config.ts
@@ -14,21 +14,21 @@ module.exports = {
   },
   networks: {
     rinkeby: {
-      url: `https://eth-ropsten.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY_RINKEBY}`,
+      url: `https://eth-ropsten.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY_RINKEBY || ''}`,
       accounts: {
-        mnemonic: process.env.RINKEBY_MNEMONIC
+        mnemonic: process.env.RINKEBY_MNEMONIC || ''
       }
     },
     ropsten: {
-      url: `https://eth-ropsten.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY_ROPSTEN}`,
+      url: `https://eth-ropsten.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY_ROPSTEN || ''}`,
       accounts: {
-        mnemonic: process.env.ROPSTEN_MNEMONIC
+        mnemonic: process.env.ROPSTEN_MNEMONIC || ''
       }
     },
     hardhat: {
       forking: {
-        url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY_MAINNET}`,
-        enabled: true,
+        url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY_MAINNET || ''}`,
+        enabled: false,
       },
     },
   },
